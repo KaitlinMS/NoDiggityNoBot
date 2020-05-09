af89f4fb637ea075e959be844a728a7e950116cb
kaitlin <kaitlin@kaitlins-mbp-10.lan>
2020-05-08 23:27:37 -0400

    Update bot.py
    Brackets are hard.

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ client = discord.Client();
 
 @client.event
 async def on_ready():
-  print ('We have logged in as {user.0}').format(client())
+  print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message():
