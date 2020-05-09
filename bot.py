ace13f20d82bb2170b1b3eb8479cbafb9209bca5
kaitlin <kaitlin@kaitlins-mbp-10.lan>
2020-05-09 01:02:29 -0400

    Update bot.py
    Remove some debugging statements.

--- bot.py
+++ bot.py
@@ -5,18 +5,14 @@ client = discord.Client();
 
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+  print('Successfully logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
-    print('There was a message!')
-    print(message.content)
     if message.author == client.user:
-        print('Message from self - ignore!')
         return
 
     if message.content.startswith('hello'):
-        print('I see a hello!')
         await message.channel.send('Hello!')
 
 client.run('NzA4NTExMjc1NjQxOTk1Mjg1.XrYa7Q.CsHp1Qym_MWG6t233YdZvGAldcU')
