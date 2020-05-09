9b3ea589ab05585b1283c2e3dec3d4510dd14bba
kaitlin <kaitlin@kaitlins-mbp-10.lan>
2020-05-09 01:02:46 -0400

    Update bot.py
    Announce who vetoed a movie!

--- bot.py
+++ bot.py
@@ -15,4 +15,9 @@ async def on_message(message):
     if message.content.startswith('hello'):
         await message.channel.send('Hello!')
 
+@client.event
+async def on_reaction_add(reaction, user):
+  if reaction.emoji == '💩' and reaction.count == 1:
+    await reaction.message.channel.send('{0} has been vetoed by ||{1}||!'.format(reaction.message.content, reaction.message.author.name))
+
 client.run('NzA4NTExMjc1NjQxOTk1Mjg1.XrYa7Q.CsHp1Qym_MWG6t233YdZvGAldcU')
